4e772c66a51295826b8e1c2ddf25759fd37a71e2
Monika Bisht <monikabisht59@gmail.com>
2021-06-20 01:48:32 +0530

    reformatted code as per PEP guidelines and included cProfiling

--- Task_1/Task_1_Processing.py
+++ Task_1/Task_1_Processing.py
@@ -1,20 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+__author__ = 'monika_bisht'
+
+# importing pandas package for usage
 import pandas as pd
 
-def readdata(name):
-    return (pd.read_excel(name,sheet_name=None))
 
-def writecsv(data,pattern,name):
-    list_df = []
-    for i in data.keys():
-        if i.startswith(pattern):
-            list_df.append(data[i])
-    pd.concat(list_df).to_csv(name,index=False)
+def read_data(file):
+    """
+    This function reads an excel file with any number of sheets and convert it to a dataframe.
+    :param file: complete path of source data file (excel) to be read
+    :return: pandas dataframe having data from all sheets from the excel
+    """
+    return pd.read_excel(file, sheet_name=None)
 
 
-if __name__ == '__main__' :
-    details = readdata('C:/Users/Monika Bisht/Desktop/data_source/data.xlsx')
-    writecsv(details,'Detail_67_','detail.csv')
-    writecsv(details,'DetailVol_67_','detailVol.csv')
-    d2 = readdata('C:/Users/Monika Bisht/Desktop/data_source/data_1.xlsx')
-    data = {**details,**d2}
-    writecsv(data,'DetailTemp_67_','detailTemp.csv')
+def write_csv(data, pattern, output_file):
+    """
+    This function filters out required sheets from the dataframe based on specific patterns in sheet names and stores
+    all that in respective csv files.
+    :param data: dataframe that needs to be converted to csv file
+    :param pattern: Pattern refers to the similar pattern in sheet names.
+    :param output_file: Complete csv file path that needs to be created
+    :return: Nothing
+    """
+    try:
+        list_df = []
+        for i in data.keys():
+            if i.startswith(pattern):
+                list_df.append(data[i])
+        pd.concat(list_df).to_csv(output_file, encoding='utf-8', index=False)
+        print("{0} file generated from data sourced from sheet name like {1}".format(output_file, pattern))
+
+    except Exception as error:
+        print("Execution failed with error: {}".format(error))
+        raise error
+
+
+def main(properties):
+    """
+    This is the main function of the program.
+    :param properties: Input properties as dictionary
+    :return: Boolean Status if run completes, else Error msg if fails.
+    """
+    try:
+        # Iterating through all source files
+        for k in range(0, len(properties['source_file_names'])):
+            file = properties['working_path'] + properties['source_file_names'][k]
+            dataframe = read_data(file)  # Reading the source file and storing the dataframe in a variable
+            print('Successfully read file {0}'.format(file))
+            # If else block is checking for iteration as post 1st iteration data read from source files get combined
+            # into 1 single dataframe
+            if k == 0:
+                overall_data = dataframe
+            else:
+                overall_data = {**data, **dataframe}
+            data = overall_data
+
+        # Once all the source files are read, selective data is written back to csv files as per the pattern found in
+        # sheet names
+        for i in range(0, len(properties['sheet_name_patterns'])):
+            write_csv(overall_data, properties['sheet_name_patterns'][i],
+                      properties['working_path'] + properties['output_csv_file_names'][i])
+        return True
+
+    except Exception as error:
+        print('Execution failed with error: {}'.format(error))
+        raise error
+
+
+if __name__ == '__main__':
+    try:
+        print('Process Started')
+        # passing all input parameters in dictionary, all Task 1 requirements passed at the same time.
+        input_properties = {
+            "working_path": "C:/Users/Monika Bisht/Desktop/data_source/",
+            "source_file_names": ['data.xlsx', 'data_1.xlsx'],
+            "sheet_name_patterns": ['Detail_67_', 'DetailVol_67_', 'DetailTemp_67_'],
+            "output_csv_file_names": ['detail.csv', 'detailVol.csv', 'detailTemp.csv'],
+        }
+        # Invoking Profiling
+        import cProfile
+
+        # Runs the main function and stores the profiling data in output.dat file
+        cProfile.run('main(input_properties)', "output.dat")
+
+        import pstats
+        from pstats import SortKey
+
+        # Reads the output.dat file and stores the result in readbale format in txt file sorted by time.
+        with open("C:/Users/Monika Bisht/Desktop/Nunam_Internship/Task_4/output_task1_time.txt", "w") as f:
+            p = pstats.Stats("output.dat", stream=f)
+            p.sort_stats("time").print_stats()
+
+        print('Process Completed')
+    except Exception as e:
+        print("Execution failed with error: {}".format(e))
+        raise e
--- Task_2/Task_2_Processing.py
+++ Task_2/Task_2_Processing.py
@@ -1,32 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+__author__ = 'monika_bisht'
+
+# importing pandas package for usage
 import pandas as pd
 
-def downsampledet(data,col):
-    m = data.iloc[:, 4:].resample("T").mean()
-    n = data.iloc[:, 0:4]
-    res = pd.merge(n, m, how="inner", on=col)
-    return res
 
-def downsample(data,col):
-    m = data.iloc[:, 2:].resample("T").mean()
-    n = data.iloc[:, 0:2]
-    res = pd.merge(n, m, how="inner", on=col)
-    return res
+def down_sampling(data_frame, key_column):
+    """
+    This function reduces the sampling of provided data from 1 sample/second to 1 sample/minute.
+    :param data_frame: Pandas Dataframe that needs to resampled
+    :param key_column: DateTime column on which the sampling will be based on
+    :return: Downsampled dataframe
+    """
+    min_time_entry = data_frame[key_column].min()
+    max_time_entry = data_frame[key_column].max()
+    time_series = pd.Series(pd.date_range(min_time_entry, max_time_entry, freq='T'))
+    date_time_df = time_series.to_frame(name=key_column)
+    return pd.merge(data_frame, date_time_df, how="inner", on=key_column)
 
-def readdata(name,col):
-    d = pd.read_csv(name,parse_dates = [col],index_col=0)
-    # d.drop(d.columns[0],axis = 1,inplace=True)
-    return (d.set_index([col]))
 
-def writecsv(data,name):
-    data.to_csv(name)
+def read_data(file_name, key_column):
+    """
+    This function reads a csv file
+    :param file_name: Complete input file path of csv file
+    :param key_column: DateTime Column on which sampling is based on
+    :return: dataframe
+    """
+    return pd.read_csv(file_name, parse_dates=[key_column], index_col=0, encoding='utf-8')
+
+
+def write_csv(final_df, output_file):
+    """
+    This function write a dataframe to csv file
+    :param final_df: Dataframe
+    :param output_file: Complete Output file path of csv file
+    :return: Nothing
+    """
+    return final_df.to_csv(output_file, encoding='utf-8', index=False)
+
+
+def main(properties):
+    """
+    This is the main function of the program.
+    :param properties: Input properties as dictionary
+    :return: Boolean Status if run completes, else Error msg if fails.
+    """
+    try:
+        # Iterating through all source files
+        for k in range(0, len(properties["source_file_names"])):
+            file = properties["working_path"] + properties["source_file_names"][k]
+            key_column = properties["key_column_name"][k]
+            output_file_name = properties["working_path"] + properties["output_csv_file_names"][k]
+            source_data = read_data(file, key_column)
+            down_sampled_data = down_sampling(source_data, key_column)
+            write_csv(down_sampled_data, output_file_name)
+
+    except Exception as error:
+        print("Execution failed with error: {}".format(error))
+        raise error
+
 
 if __name__ == '__main__':
-    detail = readdata('C:/Users/Monika Bisht/Desktop/Nunam_Internship/Task_1/detail.csv', 'Absolute Time')
-    data = downsampledet(detail, 'Absolute Time')
-    writecsv(data,'detailDownsampled.csv')
-    detailVol = readdata('C:/Users/Monika Bisht/Desktop/Nunam_Internship/Task_1/detailVol.csv', 'Realtime')
-    data = downsample(detailVol, 'Realtime')
-    writecsv(data,'detailVolDownsampled.csv')
-    detailTemp = readdata('C:/Users/Monika Bisht/Desktop/Nunam_Internship/Task_1/detailTemp.csv', 'Realtime')
-    data = downsample(detailTemp, 'Realtime')
-    writecsv(data, 'detailTempDownsampled.csv')
+    try:
+        print('Process Started')
+        # passing all input parameters in dictionary, all Task 1 requirements passed at the same time.
+        input_properties = {
+            "working_path": "C:/Users/Monika Bisht/Desktop/data_source/",
+            "source_file_names": ['detail.csv', 'detailVol.csv', 'detailTemp.csv'],
+            "key_column_name": ['Absolute Time', 'Realtime', 'Realtime'],
+            "output_csv_file_names": ['detailDownsampled.csv', 'detailVolDownsampled.csv', 'detailTempDownsampled.csv'],
+        }
+        # Invoking Profiling
+        import cProfile
+
+        # Runs the main function and stores the profiling data in output.dat file
+        cProfile.run('main(input_properties)', "output.dat")
+
+        import pstats
+        from pstats import SortKey
+
+        # Reads the output.dat file and stores the result in readbale format in txt file sorted by time.
+        with open("C:/Users/Monika Bisht/Desktop/Nunam_Internship/Task_4/output_task2_time.txt", "w") as f:
+            p = pstats.Stats("output.dat", stream=f)
+            p.sort_stats("time").print_stats()
+
+        print('Process Completed')
+    except Exception as e:
+        print("Execution failed with error: {}".format(e))
+        raise e
